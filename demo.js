ad000dcb83aba79886fa6d5f5e34fb39bccbddda
Joel Parker Henderson <joel@joelparkerhenderson.com>
2021-10-01 09:31:00 -0400

    Fix comments

--- demo.js
+++ demo.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
-##
-# Demo of Selenium browser automation with JavaScript
-# Please see the file README.md for more information.
-#
-# ## Tracking
-#
-#   * Package: demo-selenium-javasript
-#   * Version: 1.1.0
-#   * Created: 2019-11-02T00:00:00Z
-#   * Updated: 2021-10-01T13:05:10Z
-#   * License: GPL-2.0-or-greater or for custom license contact us
-#   * Contact: Joel Parker Henderson (joel@joelparkerhenderson.com)
-##
+///
+// Demo of Selenium browser automation with JavaScript
+// Please see the file README.md for more information.
+//
+// ## Tracking
+//
+//   * Package: demo-selenium-javasript
+//   * Version: 1.1.0
+//   * Created: 2019-11-02T00:00:00Z
+//   * Updated: 2021-10-01T13:05:10Z
+//   * License: GPL-2.0-or-greater or for custom license contact us
+//   * Contact: Joel Parker Henderson (joel@joelparkerhenderson.com)
+///
 
 // Initialize Selenium Webdriver, which is the manager
 const {By,Key,Builder} = require("selenium-webdriver");
